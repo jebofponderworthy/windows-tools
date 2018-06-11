1d895d9a3f1e866368f2038de5ad767ecadae3ab
Jonathan E. Brickman <jeb@ponderworthy.com>
2018-06-11 11:22:33 -0500

    Progress

--- tools/GetRedists.ps1
+++ tools/GetRedists.ps1
@@ -16,7 +16,7 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
 
 $reportStatus = ''
 $currentOp = ''
-function ShowCATEProgress {
+function ShowProgress {
 	param( [string]$reportStatus, [string]$currentOp )
 
     Write-Progress -Activity "Get Microsoft Redists" -Status $reportStatus -PercentComplete -1 -CurrentOperation $currentOp
@@ -24,23 +24,24 @@ function ShowCATEProgress {
 $wco = (New-Object System.Net.WebClient)
 $wco.Encoding = [System.Text.Encoding]::UTF8
 
-ShowCATEProgress("Preparing Powershell environment:","Installing NuGet Package Provider for VcRedist...")
+ShowProgress("Preparing Powershell environment:","Installing NuGet Package Provider for VcRedist...")
 Install-PackageProvider -Name NuGet -Force
-ShowCATEProgress("Preparing Powershell environment:","Installing NuGet for VcRedist...")
+ShowProgress("Preparing Powershell environment:","Installing NuGet for VcRedist...")
 Install-Module -Name NuGet -SkipPublisherCheck -Force
-ShowCATEProgress("Preparing Powershell environment:","Importing NuGet for VcRedist...")
+ShowProgress("Preparing Powershell environment:","Importing NuGet for VcRedist...")
 Import-Module -Name NuGet
-ShowCATEProgress("Preparing Powershell environment:","Installing VcRedist...")
+ShowProgress("Preparing Powershell environment:","Installing VcRedist...")
 Install-Module -Name VcRedist -SkipPublisherCheck -Force
-ShowCATEProgress("Preparing Powershell environment:","Importing VcRedist...")
+ShowProgress("Preparing Powershell environment:","Importing VcRedist...")
 Import-Module -Name VcRedist
-ShowCATEProgress("Preparing filesystem","")
+ShowProgress("Preparing filesystem","")
 New-Item C:\VcRedist -ItemType Directory | Out-Null
-ShowCATEProgress("Retrieving all redistributables...","")
+ShowProgress("Retrieving all redistributables...","")
 Get-VcList | Get-VcRedist -Path C:\VcRedist | Out-Null
-ShowCATEProgress("Installing all redistributables...","")
+ShowProgress("Installing all redistributables...","")
 Get-VcList | Install-VcRedist -Path C:\VcRedist | Out-Null
-ShowCATEProgress("Removing C:\VcRedist ...","")
+ShowProgress("Removing C:\VcRedist ...","")
 Remove-Item C:\VcRedist -Recurse -Force | Out-Null
-ShowCATEProgress("Done!","")
+ShowProgress("Done!","")
+
 
