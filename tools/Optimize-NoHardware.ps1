dd7e57067516e654ce14f2d2a274e19f3a9d5def
Jonathan E. Brickman <jonathanb@centuryks.com>
2021-09-08 11:22:32 -0500

    Updates

---
+++ tools/Optimize-NoHardware.ps1
@@ -0,0 +1,49 @@
+
+if (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+    {
+    "Running elevated; good."
+    ""
+    }
+else {
+    "Not running as elevated.  Starting elevated shell."
+    Start-Process powershell -WorkingDirectory $PSScriptRoot -Verb runAs -ArgumentList "-noprofile -noexit -file $PSCommandPath"
+    return "Done. This one will now exit."
+    ""
+    }
+	
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+Import-Module BitsTransfer
+
+$ps_script_list = @(
+    'mma-appx-etc.ps1',
+    'TweakMemTCP.ps1',
+    'RunDevNodeClean.ps1',
+    'wt_removeGhosts.ps1',
+    'TweakDrives.ps1',
+	'TweakSMB.ps1',
+    'OWTAS.ps1',
+	'TOSC.ps1',
+    'OVSS.ps1',
+    'CATE.ps1'
+    )
+
+$wco = (New-Object System.Net.WebClient)
+
+ForEach ($ps_script in $ps_script_list) {
+	$download_url = "https://github.com/jebofponderworthy/windows-tools/raw/master/tools/$ps_script"
+
+	""
+	"--- Downloading $ps_script... ---"
+	[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+	Invoke-WebRequest -Uri $download_url -Outfile ".\$ps_script"
+	
+	$run_script = ".\$ps_script"
+
+	& $run_script
+	Remove-Item ".\$ps_script"
+	}
+	
+$wco.Dispose()
+
+exit
