b8f56fc63917035f780bd51f4d42c1bef30ef064
Jonathan E. Brickman <jonathanb@centuryks.com>
2021-09-10 14:50:12 -0500

    Update OVSS.ps1

--- tools/OVSS.ps1
+++ tools/OVSS.ps1
@@ -1,7 +1,7 @@
 
 <#PSScriptInfo
 
-.VERSION 3.8
+.VERSION 4.0
 
 .GUID ced41cc3-0763-4229-be97-4aac877c39e2
 
@@ -102,7 +102,7 @@ Param()
 #
 # by Jonathan E. Brickman
 #
-# Removes all orphan shadows, and then preallocates 40%
+# Removes all orphan shadows if possible, and then preallocates 40%
 # of each drive volume for VSS as many different tools'
 # docs advise.  
 #
@@ -111,7 +111,32 @@ Param()
 # anymore, because security tools are flagging this as
 # a violation.
 #
-# Copyright 2020 Jonathan E. Brickman
+# As of 4.0, applying VSS tweaks:
+#
+# HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\VSS\Settings
+# DWORD MaxShadowCopies
+# Microsoft-reported default is 64.  Minimum 1, maximum 512.
+# Factually observed default appears much larger than 64.
+# Setting to 32.
+#
+# HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\VolSnap
+# DWORD MinDiffAreaFileSize
+# No known default exists.  Must not be larger than MaxSize as set
+# by VSSADMIN.
+# Would like to set to 15% of minimum drive size, given that MaxSize seems to default
+# at either unlimited or 20% depending on reference taken.  But 
+# this is a system-wide setting for all drives at once!
+# Also has to be a multiple of 32, as in 32 megabytes.
+# We're not seeing drives smaller than 100G at this writing, so will use
+# 10% of that, which is 10G, 10000M, nearest multiple of 32 is 32*312=9984.
+# Hopefully not a problem for small flash drives.
+#
+# The best references for this thus far appear to be:
+# https://docs.microsoft.com/en-us/windows/win32/backup/registry-keys-for-backup-and-restore
+# https://support.microsoft.com/kb/945058
+# https://support.microsoft.com/en-us/topic/mindiffareafilesize-registry-value-limit-is-increased-from-3-gb-to-50-gb-in-windows-8-1-or-windows-server-2012-r2-fbc32c81-1a4e-787b-ca7f-892225cd07e9
+#
+# Copyright 2021 Jonathan E. Brickman
 # https://notes.ponderworthy.com/
 # This script is licensed under the 3-Clause BSD License
 # https://opensource.org/licenses/BSD-3-Clause
@@ -148,7 +173,7 @@ $VSSVolumesData = (vssadmin list volumes)
 ForEach ($DataLine in $VSSVolumesData) {
     If ((-join $DataLine[0..12]) -eq "Volume path: ") {
         $VolumeID = (-join $DataLine[13..60])
-        "Setting for: " + $VolumeID
+        "Setting MaxSize and clearing for: " + $VolumeID
         ""
 		
 		$ForString = "/For=$VolumeID"
@@ -170,4 +195,95 @@ ForEach ($DataLine in $VSSVolumesData) {
         }
     }
 
+"Adjusting general VSS settings:"
+""
+
+function setupDWORD {
+    param( [string]$regPath, [string]$nameForDWORD, [long]$valueForDWORD )
+
+    ##############
+    # Error out if cannot touch the registry area at all
+    If ( !(Test-Path $regPath) ) {
+        Try {
+            New-Item $regPath -Force -ErrorAction SilentlyContinue
+            }
+        Catch {
+            Write-Error ("Could not visit or create registry path " + $regPath)
+            Return
+            }
+        }
+
+    #############
+    # If an existing registry entry exists, store its value to report later
+    Try {
+        $oldValueProperty = Get-ItemProperty -Path $regPath -Name $nameForDWORD -ErrorAction SilentlyContinue
+        $oldValue = $oldValueProperty.$nameforDWORD
+        }
+    Catch {
+        $oldValue = ""
+        }
+
+    #############
+    # Report the changes to make
+    Write-Output ("DWORD to write: " + $nameForDWORD)
+    Write-Output ("at registry path " + $regPath)
+    If ($oldValue -ne "") {
+        Write-Output ("Original value is " + $oldValue)
+        }
+    else {
+        Write-Output "No original present."
+        }
+    Write-Output ("New value is " + $valueforDWORD)
+
+    ############
+    # Report no changes to make, set new registry entry, or error out
+	If ($oldValue -eq $valueforDWORD) {
+		Write-Output "No change to make."
+		""
+		Return
+		}
+    Try {
+        New-ItemProperty -Path $regPath -Name $nameForDWORD -Value $valueForDWORD -PropertyType DWORD -Force -ErrorAction SilentlyContinue > $null
+        }
+    Catch {
+        Write-Error "Failed!"
+        ""
+        Return
+        }
+
+    "Succeeded!"
+    ""
+    }
+
+<# For pasting:
+
+$New-MaxShadowCopies = 32
+$New-MinDiffAreaFileSize = 3200
+
+CD HKLM:\System\CurrentControlSet\Services\VSS\Settings
+New-ItemProperty -Path . -Name "MaxShadowCopies" -Value $New-MaxShadowCopies
+
+CD HKLM:\System\CurrentControlSet\Services\VolSnap
+New-ItemProperty -Path . -Name "MinDiffAreaFileSize" -Value $New-MinDiffAreaFileSize
+
+Restart-Service -Force -Name "VSS"
+
+#>
+
+$NewMaxShadowCopies = 32
+$NewMinDiffAreaFileSize = 3200
+
+setupDWORD 'HKLM:\System\CurrentControlSet\Services\VSS\Settings' 'MaxShadowCopies' $NewMaxShadowCopies
+setupDWORD 'HKLM:\System\CurrentControlSet\Services\VolSnap' 'MinDiffAreaFileSize' $NewMinDiffAreaFileSize
+
+""
+
+"Restarting VSS..."
+
+Restart-Service -Force -Name "VSS"
+
+""
+
+"Complete!"
+""
 
